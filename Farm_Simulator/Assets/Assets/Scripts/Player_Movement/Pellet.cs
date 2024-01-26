2eabbab9f07d81802b15f94a4c312d1c71b11bf4
BobbyRay310 <148601586+BobbyRay310@users.noreply.github.com>
2024-01-26 15:48:49 -0800

    Bullet Changes and some new scripts

---
+++ Farm_Simulator/Assets/Assets/Scripts/Player_Movement/Pellet.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Pellet : MonoBehaviour
+{
+    private Vector3 shootDir;
+
+    public void Setup(Vector3 shootDir)
+    {
+        this.shootDir = shootDir;
+    }
+    private void Update()
+    {
+        float moveSpeed = 100f;
+        transform.position += shootDir * moveSpeed * Time.deltaTime;
+
+    }
+}
