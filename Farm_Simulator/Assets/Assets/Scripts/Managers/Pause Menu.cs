e1474e1dc4447c765e702bd05e1733b93113c86c
BobbyRay310 <148601586+BobbyRay310@users.noreply.github.com>
2024-02-06 18:45:55 -0800

    Added Comments to the Pause Menu Code

--- Farm_Simulator/Assets/Assets/Scripts/Managers/Pause Menu.cs
+++ Farm_Simulator/Assets/Assets/Scripts/Managers/Pause Menu.cs
@@ -17,7 +17,7 @@ public class PauseMenu : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() //This checks to see if the player presses the Esc/Escape Key
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
@@ -34,27 +34,27 @@ public class PauseMenu : MonoBehaviour
 
     }
 
-    public void PauseGame()
+    public void PauseGame() //This function makes everything ingame freeze
     {
         pauseMenu.SetActive(true);
         Time.timeScale = 0f;
         isPaused = true;
 
     }
-    public void ResumeGame()
+    public void ResumeGame() //This enables the game to presume back to normal
     {
         pauseMenu.SetActive(false);
         Time.timeScale = 1f;
         isPaused = false;
     }
 
-    public void MainMenu()
+    public void MainMenu() //This function loads back to the Main Menu Scene
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene("Start_Menu");
     }
 
-    public void QuitGame()
+    public void QuitGame() //This quits the application/game
     {
         Application.Quit();
     }
